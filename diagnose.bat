e02d9d0dffbf0944a238d85ffdbcf78a6b870042
subhanfaisal165-CS <subhanfaisal165@gmail.com>
2026-01-06 09:56:20 +0500

    Cleanup debug scripts

--- diagnose.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git status --no-pager > git_info.txt
-git log -1 --no-pager >> git_info.txt
-git remote -v >> git_info.txt
-exit
--- verify_push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git status --no-pager > verify.txt
-echo --- >> verify.txt
-git branch -vv --no-pager >> verify.txt
